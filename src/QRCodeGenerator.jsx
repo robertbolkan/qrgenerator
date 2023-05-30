48405a4b64ab6be0534dc0860b17f07cdc06dd3e
Robert Bolkan <robert.bolkan@yahoo.com>
2023-05-29 20:51:51 -0400

    changed receiver.css

--- src/QRCodeGenerator.jsx
+++ src/QRCodeGenerator.jsx
@@ -49,8 +49,8 @@ const QRCodeGenerator = () => {
   };
 
   const handleGenerateClick = () => {
-    // const baseURL = 'http://qrbies.com.s3-website-us-west-2.amazonaws.com/receiver'; // Change this to your live site's URL
-    const baseURL = 'http://127.0.0.1:5173/receiver'; // Change this to your live site's URL
+    const baseURL = 'http://qrbies.com.s3-website-us-west-2.amazonaws.com/receiver'; // Change this to your live site's URL
+    // const baseURL = 'http://127.0.0.1:5173/receiver'; // Change this to your live site's URL
 
     const queryParams = new URLSearchParams({
       bgColor,
