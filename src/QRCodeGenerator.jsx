3d49cc3854ece7ce9173a118fc4e65f25a6ee116
Robert Bolkan <robert.bolkan@yahoo.com>
2023-05-29 21:27:41 -0400

    added cloudfront distro

--- src/QRCodeGenerator.jsx
+++ src/QRCodeGenerator.jsx
@@ -49,7 +49,7 @@ const QRCodeGenerator = () => {
   };
 
   const handleGenerateClick = () => {
-    const baseURL = 'http://qrbies.com.s3-website-us-west-2.amazonaws.com/receiver'; // Change this to your live site's URL
+    const baseURL = 'https://d14zqy8hk32xke.cloudfront.net/receiver'; // Change this to your live site's URL
     // const baseURL = 'http://127.0.0.1:5173/receiver'; // Change this to your live site's URL
 
     const queryParams = new URLSearchParams({
